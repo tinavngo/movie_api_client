37e7dc4efce3f69e63cacc930dfe6039d03a128e
tinavngo <143037396+tinavngo@users.noreply.github.com>
2024-03-05 10:53:06 -0800

    Update user & token

--- components/main-view/main-view.jsx
+++ components/main-view/main-view.jsx
@@ -8,10 +8,11 @@ import { SignupView } from "../signup-view/signup-view";
 export const MainView = () => {
     const storedUser = JSON.parse(localStorage.getItem("user"));
     const storedToken = localStorage.getItem("token");
+
     const [movies, setMovies] = useState([]);
     const [selectedMovie, setSelectedMovie] = useState(null);
-    const [user, setUser] = useState(null);
-    const [token, setToken] = useState(null);
+    const [user, setUser] = useState(storedUser? storedUser : null);
+    const [token, setToken] = useState(storedToken? storedToken : null);
       
     useEffect(() => {
       if (!token) {

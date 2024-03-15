fd3ebfaf50c306f94e3798b37871e8759fd4ef2f
tinavngo <143037396+tinavngo@users.noreply.github.com>
2024-03-14 20:57:27 -0700

    Bootstrap Login-View

--- components/login-view/login-view.jsx
+++ components/login-view/login-view.jsx
@@ -1,4 +1,5 @@
 import { React, useState } from "react";
+import { Button, Form, Container, Row, Col, CardGroup, Card } from "react-bootstrap";
 
 export const LoginView = ({ onLoggedIn }) => {
     const [username, setUsername] = useState("");
@@ -15,50 +16,65 @@ export const LoginView = ({ onLoggedIn }) => {
         fetch("https://tinflicks-2bf7ff98613b.herokuapp.com/login", {
             method: "POST",
             headers: {
-                "Content-Type" : "application/json"
+                "Content-Type": "application/json"
             },
             body: JSON.stringify(data)
         })
-        .then((response) => response.json())
-          .then((data) => {
-            console.log("Login response: ", data);
-            if (data.user) {
-                localStorage.setItem("user", JSON.stringify(data.user));
-                localStorage.setItem("token", data.token);
-                onLoggedIn(data.user, data.token);
-            } else {
-                alert("No such user");
-            }
-          })
-          .catch((e) => {
-            alert("Something went wrong");
-          });
+            .then((response) => response.json())
+            .then((data) => {
+                console.log("Login response: ", data);
+                if (data.user) {
+                    localStorage.setItem("user", JSON.stringify(data.user));
+                    localStorage.setItem("token", data.token);
+                    onLoggedIn(data.user, data.token);
+                } else {
+                    alert("No such user");
+                }
+            })
+            .catch((e) => {
+                alert("Something went wrong");
+            });
     };
 
     return (
-        <form onSubmit={handleSubmit}>
-            <label>
-                Username:
-                <input
-                type="text"
-                value={username}
-                onChange={(e) => setUsername(e.target.value)}
-                required
-                 />
-            </label>
-            <br></br>
-            <label>
-                Password:
-                <input 
-                type="password"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-                required
-                 />
-            </label>
-            <br></br>
-            <button 
-            type="submit">Login</button>
-        </form>
+        <Container>
+            <Row>
+                <Col>
+                    <CardGroup>
+                        <Card className="h-100">
+                            <Card.Body>
+                                <Form onSubmit={handleSubmit}>
+                                <h1>Login</h1>
+                                <br />
+                                    <Form.Group controlId="formusername">
+                                        <Form.Label>Username:</Form.Label>
+                                        <Form.Control
+                                            type="text"
+                                            value={username}
+                                            onChange={(e) => setUsername(e.target.value)}
+                                            required
+                                            minLength="3"
+                                        />
+                                    </Form.Group>
+                                    <Form.Group controlId="formPassword">
+                                        <Form.Label>Password:</Form.Label>
+                                        <Form.Control
+                                            type="password"
+                                            value={password}
+                                            onChange={(e) => setPassword(e.target.value)}
+                                            required
+                                        />
+                                    </Form.Group>
+                                    <br />
+                                    <Button varient="primary" type="submit">
+                                        Login
+                                    </Button>
+                                </Form>
+                            </Card.Body>
+                        </Card>
+                    </CardGroup>
+                </Col>
+            </Row>
+        </Container>
     );
 };

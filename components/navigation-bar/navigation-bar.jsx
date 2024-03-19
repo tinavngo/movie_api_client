eee82440b4ba38c92fc4e7a26f363782a2d52d30
tinavngo <143037396+tinavngo@users.noreply.github.com>
2024-03-19 16:00:09 -0700

    Add NavigationBar Dropdown

--- components/navigation-bar/navigation-bar.jsx
+++ components/navigation-bar/navigation-bar.jsx
@@ -1,10 +1,10 @@
-import { Navbar, Container, Nav } from "react-bootstrap";
+import { Navbar, Container, Nav, NavDropdown, Form, Button } from "react-bootstrap";
 import React from "react";
 import { Link } from "react-router-dom";
 
 export const NavigationBar = ({ user, onLoggedOut }) => {
     return (
-        <Navbar bg="light" expand="lg">
+        <Navbar className="bg-body-tertiary" expand="lg">
             <Container>
                 <Navbar.Brand as={Link} to="/">
                     TinFlicks
@@ -14,26 +14,32 @@ export const NavigationBar = ({ user, onLoggedOut }) => {
                     <Nav className="me-auto">
                         {!user && (
                             <>
-                            <Nav.Link as={Link} to="/login">
-                                Login
+                                <Nav.Link as={Link} to="/login">
+                                    Login
                                 </Nav.Link>
-                            <Nav.Link as={Link} to="/signup">
-                                Sign up
-                            </Nav.Link>
-                                </>
+                                <Nav.Link as={Link} to="/signup">
+                                    Sign up
+                                </Nav.Link>
+                            </>
                         )}
                         {/* After login */}
                         {user && (
                             <>
-                            <Nav.Link as={Link} to="/">
-                                Home
-                            </Nav.Link>
-                            <Nav.Link as={Link} to="/profile">
-                                Profile
-                            </Nav.Link>
-                            <Nav.Link onClick={onLoggedOut}>
-                                Logout
-                            </Nav.Link>
+                                <Nav.Link as={Link} to="/">
+                                    Home
+                                </Nav.Link>
+                                <NavDropdown title="Profile" id="navbarScrollingDropdown">
+                                    <Nav.Link as={Link} to="/account">
+                                        Account
+                                    </Nav.Link>
+                                    <Nav.Link as={Link} to="/favorites">
+                                        Favorites
+                                    </Nav.Link>
+                                    <NavDropdown.Divider />
+                                    <Nav.Link onClick={onLoggedOut}>
+                                        Logout
+                                    </Nav.Link>
+                                </NavDropdown>
                             </>
                         )}
                     </Nav>

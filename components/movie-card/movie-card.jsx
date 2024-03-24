d5f9a28f6e98b114cd73f2d6a40a188579a8f458
tinavngo <143037396+tinavngo@users.noreply.github.com>
2024-03-24 13:28:44 -0700

    Add React Icon
    React Icon used for favorites on movie card

--- components/movie-card/movie-card.jsx
+++ components/movie-card/movie-card.jsx
@@ -1,19 +1,35 @@
 import PropTypes from "prop-types";
 import React from "react";
-import { Card } from "react-bootstrap";
 import "./movie-card.scss";
+import { Card } from "react-bootstrap";
 import { Link } from "react-router-dom";
+import { HeartFill, Heart } from "react-bootstrap-icons";
 
 //Component prop here
-export const MovieCard = ({ movie }) => {
+export const MovieCard = ({ movie, isFavorite, addFavorite, removeFavorite }) => {
     return (
-        <Card variant="link" className="h-100">
-            <Link to={`/movies/{encodeURIComponent(movie._id)}`}>
+        <Card variant="link" className="movie-card h-100">
+            <Link to={`/movies/${encodeURIComponent(movie._id)}`}>
             <Card.Img variant="top" src={movie.ImagePath} />
             </Link>
             <Card.Body>
                 <Card.Title>{movie.Title}</Card.Title>
                 <Card.Text>{movie.Director.Name}</Card.Text>
+                    {isFavorite ? (
+                        <HeartFill 
+                        size={30} 
+                        color="white"
+                        className="heart-button1 mt-2 me-2 top-0 end-0 border border-secondary p-1"
+                        onClick={() =>
+                        removeFavorite(movie._id)}/>
+                    ) : (
+                        <Heart 
+                        size={30} 
+                        color="orange"
+                        className="heart-button mt-2 me-2 top-0 end-0 border border-secondary p-1"
+                        onClick={() => 
+                        addFavorite(movie._id)}/>
+                    )}
             </Card.Body>
         </Card>
     );
